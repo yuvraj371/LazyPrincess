84211e9d84d525326f0c018990bb789678542bf5
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-09-17 18:16:29 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('🤥 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('🔔 Updates', url='https://t.me/Infinity_XBotz')
+            InlineKeyboardButton('🔔 Updates', url='https://t.me/Streamz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
