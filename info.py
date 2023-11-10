105cb0192d46bab77109bbb63d98d6000a91ca1f
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-11-10 09:37:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Autodeletebot:Dharamveer1100@cluster0.luyy59m.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Autodeletebot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://filterbot:dveer178@filterbot.l3zxenf.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "filterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
